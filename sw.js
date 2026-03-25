4fa94ddda185e257b82642ac8cafd7c43ab3fb17
Alvearmarcial <labor.ia.torio@gmail.com>
2026-03-25 00:12:00 -0300

    Update cache version from v2 to v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'comunidad-v2';
+const CACHE = 'comunidad-v3';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e =>
