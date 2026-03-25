2aa5ddeb56af7c42a2d51fb1664627f203f6a98c
Alvearmarcial <labor.ia.torio@gmail.com>
2026-03-25 00:35:52 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'comunidad-v6';
+const CACHE = 'comunidad-v7';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e =>
